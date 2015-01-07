b12744e68046f030ed6c4326dc005c1360c3adf9
Andrew McGill <arm5077@gmail.com>
2015-01-06 22:36:45 -0500

    Add static page

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 express = require("express");
 var app = express();
 
-app.get("/", function(request, response){
-    response.status(400).json("No command given.");
+app.get("/words", function(request, response){
+    response.status(400).json("No word selected!"); 
 });
-
 
 app.get("/words/:word", function(request, response){
-    if (!request.params.name)
-        response.status(400).json("No name selected!");
+    if (request.params.word)
+        response.status(200).json("Yay you did it right");
+    else   
+        response.status(400).json("No word selected!");    
 });
+
+app.use(express.static(__dirname + "/public"));
 
 
 module.exports = app;

8e3fca17e7f3dc13d57e611ec6ffaa9c7b0ce324
Andrew McGill <arm5077@gmail.com>
2015-01-12 16:38:08 -0500

    Fully converted chart to angular

--- public/scripts.js
+++ public/scripts.js
@@ -3,26 +3,72 @@ app = angular.module("sotuApp", []);
 app.controller("sotuController", ["$scope", function($scope){
 	$scope.keywords = keywords;
 	$scope.chartHeight = 200;
-	$scope.chartWidth = $(".chart").width();
-	$scope.padding = .4;
-	console.log($(".chart").width());
+	$scope.padding = .1;
+	$scope.minHeight = 30;
+	
+	$scope.insertBar = function(keyword){
+		keyword.years.push({year:2015, count:20});
+		
+	};
+	
 	
 	$scope.barHeight = function(value, data){
-		return value / d3.max(data, function(d){ return d.count }) * $scope.chartHeight;
+		height = value / d3.max(data, function(d){ return d.count }) * $scope.chartHeight;
+		
+		if( height < $scope.minHeight )
+			return $scope.minHeight;
+		else 
+			return height;
+
 	};
 	
-	$scope.barWidth = function(value, data){	
-		return ( $scope.chartWidth / data.length ) * (1 - $scope.padding);
+	$scope.barWidth = function(value, data) {
+		return (100 / data.length) * (1 - $scope.padding);
+		//return (100 / data.length) - ($scope.padding / (data.length - 1));
 	};
-
-	app.directive("dataBarWidth", function(){
-		return function(scope, element, attr){
-			element.css({
-				width: ( width / data.length ) * (1 - padding);
-			});
-		};
-	});
-
+	
+	$scope.barTop = function(value, data){
+		if( $scope.barHeight(value, data) < $scope.minHeight )
+			return $scope.chartHeight - $scope.minHeight;
+		else 
+			return $scope.chartHeight - $scope.barHeight(value, data);
+	};	
+	
+	$scope.barLeft = function(value, data) {
+		index = data.map(function(data){ return data.year}).indexOf(value);
+		return index * $scope.barWidth(value, data) + (index * $scope.padding * (100 / (data.length -1) ) );
+	};
+	
+	$scope.barColor = function(value, data) {
+		if( value == 0 )
+			return "rgba(250,166,26,1)";
+		else
+			return "rgba(255,255,255,1)";
+	}
+	
+	$scope.barBackgroundColor = function(value, data) {
+		if( value == 0)
+			return "rgba(250,166,26,0)";
+		else
+			return "rgba(250,166,26,1)";
+	}
 	
 }]);
+
+app.directive("bar", function() {
+	return {
+		restrict: 'E',
+		link: function(scope, element, attr) {
+			element.css({
+				width: scope.barWidth(scope.year.count, scope.keyword.years) + "%",
+				height: scope.barHeight(scope.year.count, scope.keyword.years) + "px",
+				top: scope.barTop(scope.year.count, scope.keyword.years) + "px",
+				left: scope.barLeft(scope.year.year, scope.keyword.years) + "%",
+				color: scope.barColor(scope.year.count, scope.keyword.years),
+				backgroundColor: scope.barBackgroundColor(scope.year.count, scope.keyword.years)
+			});
+		}
+	};	
+	 
+});
 
